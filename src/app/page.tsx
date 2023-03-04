8ac24e4d014cab5f6fd935efbd7e26dea537923b
Ethan <ejo2117@columbia.edu>
2023-03-03 21:24:15 -0500

    Add `Heading`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.scss";
+import Heading from "@/components/Heading";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Heading tag="h1">Hello</Heading>
+      <Heading tag="h2">Hello</Heading>
+      <Heading tag="h3">Hello</Heading>
+      <Heading tag="h4">Hello</Heading>
+      <Heading tag="h5">Hello</Heading>
+      <Heading tag="h6">Hello</Heading>
+    </main>
+  );
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { FC, forwardRef, ReactNode } from "react";
+import Balancer from "react-wrap-balancer";
+import styles from "./Heading.module.scss";
 
-const Heading = () => {
-  return <div>Heading</div>;
+type Tag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+type Props = {
+  children?: ReactNode;
+  className?: string;
+  style?: Partial<CSSStyleDeclaration>;
+  tag?: Tag;
+  ref?: React.ForwardedRef<HTMLHeadingElement>;
+  withBalancer?: boolean;
 };
+
+/**
+ * Heading
+ *
+ * @tagOptions [h1, h2, h3, h4, h5, h6]
+ * @default
+ * - Size: 4rem
+ * - Weight: 500
+ * - Height: 4.8rem
+ * - Family: Platform
+ * - Margin: 0
+ */
+const Heading: FC<Props> = forwardRef(
+  (
+    { children, className, style, tag = "h1", withBalancer = false, ...props },
+    forwardedRef
+  ) => {
+    const TAG = tag;
+
+    props.ref = forwardedRef;
+
+    const newProps = {
+      ...props,
+      className: `${styles.default} ${styles[tag]} ${className}`,
+      style,
+    };
+
+    const textContent = withBalancer ? (
+      <Balancer>{children}</Balancer>
+    ) : (
+      children
+    );
+
+    return React.createElement(TAG, newProps, textContent);
+  }
+);
+
+Heading.displayName = "Heading";
 
 export default Heading;
