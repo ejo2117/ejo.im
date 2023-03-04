9cbf19f20ee41c27bbc8ee530dcb74fab938c30c
Ethan <ejo2117@columbia.edu>
2023-03-04 14:03:12 -0500

    Add UI Components

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
+  sassOptions: {
+    includePaths: [path.resolve(__dirname, "src/app")],
+    prependData: `@import 'variables.scss';`,
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,21 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "./page.module.scss";
 import Heading from "@/components/Heading";
+import Container from "@/components/ui/Container";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Heading tag="h1">Hello</Heading>
-      <Heading tag="h2">Hello</Heading>
-      <Heading tag="h3">Hello</Heading>
-      <Heading tag="h4">Hello</Heading>
-      <Heading tag="h5">Hello</Heading>
-      <Heading tag="h6">Hello</Heading>
+      <Container>
+        <Heading tag="h1">Heading 1</Heading>
+        <Heading tag="h2">Heading 2</Heading>
+        <Heading tag="h3">Heading 3</Heading>
+        <Heading tag="h4">Heading 4</Heading>
+        <Heading tag="h5">Heading 5</Heading>
+        <Heading tag="h6">Heading 6</Heading>
+      </Container>
     </main>
   );
 }
