f58e0a3f6883f36c6902a4f1fdd0354e4e37a09c
Ethan <ejo2117@columbia.edu>
2023-04-25 22:12:07 -0400

    `useAnimationFrame` can run conditionally

--- src/lib/hooks/useAnimationFrame.tsx
+++ src/lib/hooks/useAnimationFrame.tsx
@@ -1,7 +1,10 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-const useAnimationFrame = (callback?: FrameRequestCallback) => {
+const useAnimationFrame = (
+  callback?: FrameRequestCallback,
+  runCondition = true
+) => {
   const requestRef = useRef<number>();
   const timerStartRef = useRef<number>();
   const previousTimeRef = useRef<number>();
@@ -9,28 +12,29 @@ const useAnimationFrame = (callback?: FrameRequestCallback) => {
   const [timeElapsed, setTimeElapsed] = useState(0);
   const [deltaTime, setDeltaTime] = useState(0);
 
-  // Handle timer bar animations
   // There's a great explanation of this approach here https://css-tricks.com/using-requestanimationframe-with-react-hooks/
-  const step: FrameRequestCallback = (timestamp) => {
-    if (timerStartRef.current && previousTimeRef.current) {
-      setTimeElapsed(timestamp - timerStartRef.current);
-      setDeltaTime(timestamp - previousTimeRef.current);
-    }
-    previousTimeRef.current = timestamp;
-    requestRef.current = window.requestAnimationFrame(step);
-    callback && callback(timestamp);
-  };
+  const step: FrameRequestCallback = useCallback(
+    (timestamp) => {
+      if (timerStartRef.current && previousTimeRef.current) {
+        setTimeElapsed(timestamp - timerStartRef.current);
+        setDeltaTime(timestamp - previousTimeRef.current);
+      }
+      previousTimeRef.current = timestamp;
+      requestRef.current = window.requestAnimationFrame(step);
+      callback && callback(timestamp);
+    },
+    [callback]
+  );
 
   useEffect(() => {
-    // TODO - Add support for conditional execution
-    timerStartRef.current = performance.now();
-    requestRef.current = window.requestAnimationFrame(step);
-
+    if (runCondition) {
+      timerStartRef.current = performance.now();
+      requestRef.current = window.requestAnimationFrame(step);
+    }
     return () => {
       !!requestRef.current && window.cancelAnimationFrame(requestRef.current);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [runCondition, step]);
 
   return [timeElapsed, deltaTime] as const;
 };
