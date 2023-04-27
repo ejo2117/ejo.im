e05a9c289ff1c6b955991b68d596dfe8b05471c3
Ethan <ejo2117@columbia.edu>
2023-04-26 21:07:54 -0400

    `li`s in experience page

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,5 @@
 import Cell from "@/components/ui/Cell";
-import { Flex } from "@/components/ui";
+import { Caption, Flex } from "@/components/ui";
 import {
   FigmaBadge,
   GraphQlBadge,
@@ -13,38 +13,58 @@ export default function Resume() {
   // Return JSX here!
 
   return (
-    <Flex column gap={4} position="relative">
-      <Cell
-        jobTitle={"Full Stack Developer"}
-        company={"Pair Eyewear"}
-        dates={"2020 - 2023"}
-        description={`
-           
-          Cross-functional collaboration has a been a frequent and critical aspect of the role, and I have enjoyed working through challenges across multiple domains.  
-        
-          `}
-        skills={["typescript", "shopify", "react", "nextjs", "graphql"]}
-      />
-      <Cell
-        jobTitle={"UI/UX Intern"}
-        company={"Pair Eyewear"}
-        dates={"2020 - 2020"}
-        description={`
-
-            Promoted to a full time engineering role after 4 months.
-          `}
-        skills={["figma", "shopify", "react"]}
-      />
-      <Cell
-        jobTitle={"Co-Owner, Product Developer"}
-        company={"Sight"}
-        dates={"2018 - 2020"}
-        description={`
-            
-            Brought bold and exciting creative visions to life for multiple E-Commerce clients.
-          `}
-        skills={["figma", "nextjs", "shopify", "react"]}
-      />
-    </Flex>
+    <ul>
+      <li>
+        <Caption>
+          Full Stack Developer at <i>Pair Eyewear</i>
+        </Caption>
+      </li>
+      <li>
+        <Caption>
+          UI/UX Design Intern at <i>Pair Eyewear</i>
+        </Caption>
+      </li>
+      <li>
+        <Caption>
+          Co-Owner, Product Developer at <i>Sight</i>
+        </Caption>
+      </li>
+    </ul>
   );
+
+  // return (
+  //   <Flex column gap={4} position="relative">
+  //     <Cell
+  //       jobTitle={"Full Stack Developer"}
+  //       company={"Pair Eyewear"}
+  //       dates={"2020 - 2023"}
+  //       description={`
+
+  //         Cross-functional collaboration has a been a frequent and critical aspect of the role, and I have enjoyed working through challenges across multiple domains.
+
+  //         `}
+  //       skills={["typescript", "shopify", "react", "nextjs", "graphql"]}
+  //     />
+  //     <Cell
+  //       jobTitle={"UI/UX Intern"}
+  //       company={"Pair Eyewear"}
+  //       dates={"2020 - 2020"}
+  //       description={`
+
+  //           Promoted to a full time engineering role after 4 months.
+  //         `}
+  //       skills={["figma", "shopify", "react"]}
+  //     />
+  //     <Cell
+  //       jobTitle={"Co-Owner, Product Developer"}
+  //       company={"Sight"}
+  //       dates={"2018 - 2020"}
+  //       description={`
+
+  //           Brought bold and exciting creative visions to life for multiple E-Commerce clients.
+  //         `}
+  //       skills={["figma", "nextjs", "shopify", "react"]}
+  //     />
+  //   </Flex>
+  // );
 }
