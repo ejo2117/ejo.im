365103b834909f09bcea7abb8903280cd2def12c
Ethan <ejo2117@columbia.edu>
2023-06-24 21:15:07 -0400

    Hide `NavBar` to focus on Blob stuff

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -17,30 +17,32 @@ const NavBar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className={styles.container}>
-      <Title>Ethan O&apos;Neal</Title>
-      <Spacer size={0.4} />
-      <div className={styles.links}>
-        {PATHS.map(([href, name]) => {
-          const isCurrentPage = pathname === href;
-          return (
-            <Link
-              key={href}
-              href={href}
-              className={isCurrentPage ? styles.selected : ""}
-            >
-              <Caption>{name}</Caption>
-              {/* {pathname === href && (
+    null && (
+      <nav className={styles.container}>
+        <Title>Ethan O&apos;Neal</Title>
+        <Spacer size={0.4} />
+        <div className={styles.links}>
+          {PATHS.map(([href, name]) => {
+            const isCurrentPage = pathname === href;
+            return (
+              <Link
+                key={href}
+                href={href}
+                className={isCurrentPage ? styles.selected : ""}
+              >
+                <Caption>{name}</Caption>
+                {/* {pathname === href && (
                 <motion.div
                   className={styles.underline}
                   layoutId="underline"
                 ></motion.div>
               )} */}
-            </Link>
-          );
-        })}
-      </div>
-    </nav>
+              </Link>
+            );
+          })}
+        </div>
+      </nav>
+    )
   );
 };
 
