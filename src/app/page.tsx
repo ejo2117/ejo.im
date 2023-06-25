1871105b6d619481672fed2d836c1a3345ffdc82
Ethan <ejo2117@columbia.edu>
2023-06-24 21:16:20 -0400

    idk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default async function Home() {
   const poline = new Poline();
   return (
     <Flex column center gap={4}>
+      <div id="logger"></div>
       {/* <div className={styles.blobs}>
         <Blob colors={poline} blurStrength={8} points={4} />
         <Blob colors={poline} radius={60} amplitude={12} blurStrength={1} />
@@ -16,6 +17,13 @@ export default async function Home() {
       </div> */}
 
       <SoundBlob colors={poline} blurStrength={8} points={4} />
+      {/* <SoundBlob
+        colors={poline}
+        radius={110}
+        blurStrength={8}
+        points={8}
+        amplitude={20}
+      /> */}
       {/* <Blob colors={poline} blurStrength={8} points={4} /> */}
     </Flex>
   );
