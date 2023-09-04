f4c4bbbed4cb74fb5c142237a8593b7cdbb3ea8b
Ethan <ejo2117@columbia.edu>
2023-09-04 19:59:05 -0400

    Update roles

---
+++ src/app/experience/roles.ts
@@ -0,0 +1,29 @@
+const roles = [
+    {
+      title: "Senior Software Engineer",
+      company: "Pair Eyewear",
+      dates: "2023 - Present",
+    },
+    {
+      title: "Software Engineer",
+      company: "Pair Eyewear",
+      dates: "2020 - 2023",
+    },
+    {
+      title: "UI/UX Intern",
+      company: "Pair Eyewear",
+      dates: "2020 - 2020",
+    },
+    {
+      title: "Co-Owner, Product Developer",
+      company: "Sight",
+      dates: "2018 - 2020",
+    },
+    {
+      title: "Barista",
+      company: "Pret a Manger",
+      dates: "2018 - 2018",
+    },
+  ];
+
+export default roles;
