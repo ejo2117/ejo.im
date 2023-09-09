68de5554e3f51fc79fea019c059631d31afb7764
Ethan <ejo2117@columbia.edu>
2023-09-09 17:02:09 -0400

    Fix "async" client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 import styles from "./page.module.scss";
 import { Body, Flex } from "@/components/ui";
-import Blob from "@/components/Blob";
+import Blob from "@/components/ui/Blob/Blob";
 import { Poline } from "poline";
 
-export default async function Home() {
+export default function Home() {
   const poline = new Poline();
   return (
-    <Flex column center gap={4}>
       <div className={styles.blobs}>
         <Blob colors={poline} blurStrength={8} points={4} />
         <Blob colors={poline} radius={110} points={8} amplitude={20} />
         <Blob colors={poline} radius={60} amplitude={12} blurStrength={1} />
       </div>
-    </Flex>
   );
 }
