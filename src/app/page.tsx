5bfa8d72d0362392387b89ff599d84f1219fdc15
Ethan <ejo2117@columbia.edu>
2023-09-16 12:22:18 -0400

    Remove grid definitions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
-import { Flex, Title } from "@/components/ui";
+import { Flex, Spacer, Title } from "@/components/ui";
 import { About, BlobStack } from "@/components/sections";
 import Experience from "@/components/sections/Experience";
 
 export default function Home() {
   return (
-    <Flex column>
+    <Flex column position="relative" style={{ zIndex: 1 }}>
       <Title>{`Ethan O'Neal`}</Title>
+      <Spacer size={3.2}/>
       <About />
+      <Spacer size={2.4}/>
       <Experience />
       <BlobStack />
     </Flex>
