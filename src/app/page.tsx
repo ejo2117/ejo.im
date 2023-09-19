60658bd2e95e30f8e718c72105265b2b68549983
Ethan <ejo2117@columbia.edu>
2023-09-18 20:19:15 -0400

    Dark mode styling and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,19 @@
 import { Flex, Spacer, Title } from "@/components/ui";
 import { About, BlobStack } from "@/components/sections";
 import Experience from "@/components/sections/Experience";
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <Flex column position="relative" style={{ zIndex: 1 }}>
-      <Title>{`Ethan O'Neal`}</Title>
-      <Spacer size={3.2}/>
-      <About />
-      <Spacer size={2.4}/>
-      <Experience />
-      <BlobStack />
+      <Flex column className={styles.glass}>
+        <Title>{`Ethan O'Neal`}</Title>
+        <Spacer size={6.4}/>
+        <About />
+        <Spacer size={2.4}/>
+        <Experience />
+      </Flex>
+        <BlobStack />
     </Flex>
   );
 }
