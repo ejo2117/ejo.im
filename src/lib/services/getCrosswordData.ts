e462d2cd7e4922f4e4895ad163751defe835cd6f
Ethan <ejo2117@columbia.edu>
2024-01-05 23:51:00 -0600

    add response type

--- src/lib/services/getCrosswordData.ts
+++ src/lib/services/getCrosswordData.ts
@@ -6,6 +6,41 @@ const BASE_URL='https://www.nytimes.com/svc/crosswords'
 const STATS_ENDPOINT='/v3/91901606/stats-and-streaks.json'
 const ENDPOINT_PARAMS='?date_start=2014-01-01&start_on_monday=true'
 
+type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday'
+
+export type NytStatsResponse = {
+    results: {
+        stats: {
+            longest_avg_time: number,
+            longest_latest_time: number,
+            puzzles_attempted: number,
+            puzzles_solved: number,
+            solve_rate: number,
+            stats_by_day: Array<{
+                avg_denominator: number,
+                avg_time: number,
+                best_date: string,
+                best_time: number,
+                label: DayOfWeek,
+                latest_date: string,
+                latest_time: number,
+                this_weeks_time: number
+            }>
+        },
+        streaks: {
+            current_streak: number,
+            date_end: string,
+            date_start: string,
+            dates: Array<string[]>,
+            longest_streak: number,
+            vertical_streak: Array<{
+                length: number,
+                next_date: string,
+            }>
+        }
+    }
+}
+
 export async function getCrosswordData(){
     try {
         const res = await fetch(`${BASE_URL}${STATS_ENDPOINT}${ENDPOINT_PARAMS}`, {
@@ -16,7 +51,7 @@ export async function getCrosswordData(){
         })
 
         if (res.ok) {
-            const data = await res.json()
+            const data: NytStatsResponse = await res.json()
             return data
         }
     } catch (error) {
