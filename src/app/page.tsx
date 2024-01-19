aa8b1a539357d7f414ea11493a1a3daec6675266
Ethan <ejo2117@columbia.edu>
2024-01-19 00:34:30 -0500

    display badges on homepage and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,22 @@ import styles from "./page.module.scss";
 import { Body, Flex } from "@/components/ui";
 import Blob from "@/components/Blob";
 import { Poline } from "poline";
+import { EmailBadge, GithubBadge, LinkedInBadge } from "@/components/ui/Badge/Badge";
 
 export default function Home() {
   const poline = new Poline();
   return (
-    <Flex column center gap={4}>
+    <Flex center gap={4} className={styles.mobileColumn}>
       <div className={styles.blobs}>
         <Blob colors={poline} blurStrength={8} points={4} />
         <Blob colors={poline} radius={110} points={8} amplitude={20} />
         <Blob colors={poline} radius={60} amplitude={12} blurStrength={1} />
       </div>
+      <Flex gap={4}>
+        <GithubBadge />
+        <LinkedInBadge />
+        <EmailBadge />
+      </Flex>
     </Flex>
   );
 }
