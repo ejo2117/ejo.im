ae6eaa0c729134a9916e753a57080cb2b5815cce
Ethan <ejo2117@columbia.edu>
2024-09-02 23:22:15 -0400

    feat: get playlists

--- src/lib/services/spotify.ts
+++ src/lib/services/spotify.ts
@@ -23,6 +23,54 @@ type TokenSuccessResponse = {
     expires_in: number;
 }
 
+type UserPlaylistsResponse = {
+    href: string;
+    limit: number;
+    next: string;
+    offset: number;
+    previous: string;
+    total: number;
+    items: Array<Playlist>
+}
+
+type Playlist = {
+    collaborative: boolean;
+    description: string;
+    external_urls: {
+        spotify: string;
+    };
+    href: string;
+    id: string;
+    images: Array<{
+        url: string;
+        height: number;
+        width: number;
+    }>;
+    name: string;
+    owner: {
+        external_urls: {
+            spotify: string;
+        };
+        followers: {
+            href: string;
+            total: number;
+        };
+        href: string;
+        id: string;
+        type: string;
+        uri: string;
+        display_name: string;
+    };
+    public: boolean;
+    snapshot_id: string;
+    tracks: {
+        href: string;
+        total: 0;
+    };
+    type: string;
+    uri: string;
+}
+
 export const getSpotifyAccessToken = async () => {
     try {
         const res = await fetch('https://accounts.spotify.com/api/token', {
@@ -37,14 +85,48 @@ export const getSpotifyAccessToken = async () => {
         });
 
         try {
-            const data: TokenErrorResponse | TokenSuccessResponse = await res.json();
+            const data: TokenSuccessResponse = await res.json();
             return data;
         } catch (error) {
             console.error(`Error parsing JSON: ${error}`)
+            return {} as TokenSuccessResponse
         }
     
     } catch (error) {
-        console.error(`Error fetching Token: ${error}`)    
+        console.error(`Error fetching Token: ${error}`)
+        return {} as TokenSuccessResponse
     }
     
 };
+
+export const getUserPlaylists = async (userId: string, authToken: string) => {
+    if (!userId) {
+        return null
+    }
+
+    const endpoint = `https://api.spotify.com/v1/users/${userId}/playlists`
+
+    try {
+        const res = await fetch(endpoint, {
+            'method': 'GET',
+            'headers': {
+                'Authorization': `Bearer ${authToken}`,
+            } 
+        })
+
+        try {
+            const data: UserPlaylistsResponse = await res.json();
+            return getRnPlaylists(data.items);
+        } catch (error) {
+            console.error(`Error parsing JSON: ${error}`)    
+        }
+    } catch (error) {
+        console.error(`Error fetching Playlists: ${error}`)     
+    }
+
+}
+
+
+const getRnPlaylists = (playlists: Playlist[]) => {
+    return playlists.filter(p => p.name.slice(0,2) === 'rn')
+}
