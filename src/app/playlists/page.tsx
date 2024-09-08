5fc08ab48118e72463baea71f11f73a4b38a318c
Ethan <ejo2117@columbia.edu>
2024-09-08 12:11:02 -0400

    feat: show playlist images, link to spotify

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,22 +1,31 @@
-import { Caption, Container } from "@/components/ui";
+import { Caption, Container, Flex } from "@/components/ui";
 import { getMyPlaylists } from "@/lib/services/spotify";
 import Link from "next/link";
+import styles from './page.module.scss'
 
 export default async function Playlists() {
 
     const playlists = await getMyPlaylists();
 
     return (
-        <ul>
+        <div className={styles.container}>
             {playlists.map(playlist => {
+
+                const number = parseInt(playlist.name.split(' - ')[0].split('rn')[1])
+
+                if (number < 10 || Number.isNaN(number)) {
+                    return null;
+                }
+
                 return (
-                    <li key={playlist.id}>
-                        <Link href={`/playlists/${playlist.id}`}>
-                            <Caption>{playlist.name}</Caption>
+                        <Link key={playlist.id} href={`${playlist.uri}`}>
+                            <Flex column gap={2} >
+                                <img src={playlist.images[0].url}/>
+                                <Caption>{playlist.name}</Caption>
+                            </Flex>
                         </Link>
-                    </li>
                 )
             })}
-        </ul>
+        </div>
     )
 }
