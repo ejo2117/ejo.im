77cf004076cef6d98f2fd8885e4b4c84acab507d
Ethan <ejo2117@columbia.edu>
2025-02-01 16:05:33 -0800

    feat: playlists tidy up

--- src/app/playlists/page.tsx
+++ src/app/playlists/page.tsx
@@ -1,29 +1,13 @@
-'use client';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { Caption, Container, Flex } from '@/components/ui';
+import { Caption, Flex } from '@/components/ui';
 import { getMyPlaylists } from '@/lib/services/spotify';
-import Palette from '@/components/Palette';
 import styles from './page.module.scss';
 
-export default function Playlists() {
-	const [playlists, setPlaylists] = useState<Playlist[]>([]);
-
-	const [active, setActive] = useState(19);
-
-	useEffect(() => {
-		const fetcher = async () => {
-			const res = await fetch('/api/playlists');
-			const { playlists } = await res.json();
-			setPlaylists(playlists);
-		};
-
-		fetcher();
-	}, []);
+export default async function Playlists() {
+	const playlists = await getMyPlaylists();
 
 	return (
 		<>
-			{/* {playlists.length > 0 && <Palette source={playlists[active].images[0]} />} */}
 			<div className={styles.container}>
 				{playlists.map((playlist, i) => {
 					const number = parseInt(playlist.name.split(' - ')[0].split('rn')[1]);
@@ -35,16 +19,17 @@ export default function Playlists() {
 					const season = playlist.name.split(' - ')[1];
 
 					return (
-						<div
+						<Link
 							key={playlist.id}
-							// href={`${playlist.uri}`}
-							onClick={() => setActive(i)}
+							href={playlist.uri}
+							// href={`/playlists/${playlist.id}`}
+							// onClick={() => setActive(i)}
 						>
 							<Flex column gap={2}>
 								<img src={playlist.images?.[0].url} alt={`a cover image for playlist ${playlist.name}`} />
 								<Caption>{season}</Caption>
 							</Flex>
-						</div>
+						</Link>
 					);
 				})}
 			</div>
